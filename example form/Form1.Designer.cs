093756805ba76e6028ed3d84e86c556393025df2
Deshan Jayawardhana <lahirujayawardana6@gmail.com>
2024-09-12 11:56:51 +0530

    sample form application

---
+++ example form/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace example_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconvrt = new System.Windows.Forms.Button();
+            this.txtplain = new System.Windows.Forms.TextBox();
+            this.txtconvrt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtlower = new System.Windows.Forms.TextBox();
+            this.txtfirst = new System.Windows.Forms.TextBox();
+            this.txtsecnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtconct = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnconvrt
+            // 
+            this.btnconvrt.BackColor = System.Drawing.Color.Black;
+            this.btnconvrt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvrt.ForeColor = System.Drawing.Color.Yellow;
+            this.btnconvrt.Location = new System.Drawing.Point(77, 159);
+            this.btnconvrt.Name = "btnconvrt";
+            this.btnconvrt.Size = new System.Drawing.Size(95, 34);
+            this.btnconvrt.TabIndex = 4;
+            this.btnconvrt.Text = "CONVERT";
+            this.btnconvrt.UseVisualStyleBackColor = false;
+            this.btnconvrt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtplain
+            // 
+            this.txtplain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtplain.Location = new System.Drawing.Point(77, 111);
+            this.txtplain.Name = "txtplain";
+            this.txtplain.Size = new System.Drawing.Size(208, 26);
+            this.txtplain.TabIndex = 5;
+            // 
+            // txtconvrt
+            // 
+            this.txtconvrt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconvrt.Location = new System.Drawing.Point(77, 225);
+            this.txtconvrt.Name = "txtconvrt";
+            this.txtconvrt.Size = new System.Drawing.Size(208, 29);
+            this.txtconvrt.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(192, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txtlower
+            // 
+            this.txtlower.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlower.Location = new System.Drawing.Point(77, 293);
+            this.txtlower.Name = "txtlower";
+            this.txtlower.Size = new System.Drawing.Size(208, 29);
+            this.txtlower.TabIndex = 8;
+            // 
+            // txtfirst
+            // 
+            this.txtfirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfirst.Location = new System.Drawing.Point(579, 111);
+            this.txtfirst.Name = "txtfirst";
+            this.txtfirst.Size = new System.Drawing.Size(157, 26);
+            this.txtfirst.TabIndex = 9;
+            // 
+            // txtsecnd
+            // 
+            this.txtsecnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsecnd.Location = new System.Drawing.Point(579, 164);
+            this.txtsecnd.Name = "txtsecnd";
+            this.txtsecnd.Size = new System.Drawing.Size(157, 26);
+            this.txtsecnd.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(452, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Second name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(476, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "First name";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(604, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Concatanate";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtconct
+            // 
+            this.txtconct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconct.Location = new System.Drawing.Point(579, 309);
+            this.txtconct.Name = "txtconct";
+            this.txtconct.Size = new System.Drawing.Size(157, 26);
+            this.txtconct.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(404, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Concatanated Name";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(604, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtconct);
+            this.Controls.Add(this.txtsecnd);
+            this.Controls.Add(this.txtfirst);
+            this.Controls.Add(this.txtlower);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtconvrt);
+            this.Controls.Add(this.txtplain);
+            this.Controls.Add(this.btnconvrt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnconvrt;
+        private System.Windows.Forms.TextBox txtplain;
+        private System.Windows.Forms.TextBox txtconvrt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtlower;
+        private System.Windows.Forms.TextBox txtfirst;
+        private System.Windows.Forms.TextBox txtsecnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtconct;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ example form/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /* Pen blackpen = new Pen(Color.Black,5);
+        int x1 = 100, y1 = 100, x2 = 500, y2 = 100;
+        e.Graphics.DrawLine(blackpen,x1,y1,x2,y2);
+        */
+
+
+
+
+        /*private void mousePos_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePos.Text = String.Format("({0}, {1})", e.X, e.Y);
+        }
+        */
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.KeyChar = '*';
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblDanger_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            string[] names = {"Amal","Kamal", "Namal", "teena", "meena", "tania" };
+            //for (int i = 0; i < names.Length; i++) {
+            int[] num = { 10,20,30,40,50,60,70,80,90,100};
+            foreach (int i in num) {
+                
+            liboxname.Items.Add(i);
+            }
+            
+        }*/
+
+        private void mousePos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = txtplain.Text;
+            txtconvrt.Text = a.ToUpper();
+            txtlower.Text = a.ToLower();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            txtconvrt.Text = "";
+            txtplain.Text = "";
+            txtlower.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = txtfirst.Text;
+            string b = txtsecnd.Text;
+
+            txtconct.Text = string.Concat(a," "+ b);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtfirst.Text = "";
+            txtsecnd.Text = "";
+            txtconct.Text = "";
+        }
+    }
+}
